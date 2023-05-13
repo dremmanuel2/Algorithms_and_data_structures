4178953857c7b24ce56957f95cf91ea9b2250ccc
dremmanuel2 <125027736+dremmanuel2@users.noreply.github.com>
2023-05-13 15:00:17 +0800

    Update travComToFull.cpp

--- BinTreeskh/travComToFull.cpp
+++ BinTreeskh/travComToFull.cpp
@@ -1,1 +1,39 @@
-
+template <typename T>
+void BinNodeskh<T>::travLevelCompToFull()
+{
+	std::queue<BinNodePosiskh(T)> Q;
+	Q.push(this);
+	while (!Q.empty())
+	{
+		BinNodePosiskh(T)y;
+		BinNodePosiskh(T)x = Q.front();
+		if (IsRoot(*x))
+		{
+			y = NULL;
+		}
+		else
+		{
+			y = (sibling(x));
+		}
+		Q.pop();
+		if (HasLChild(*x))
+		{
+			Q.push(x->lc);
+		}
+		else if (y)
+		{
+			if (HasChild(*y))
+				Q.push(x->insertAsLC(NULL));
+			
+		}
+		if (HasRChild(*x))
+		{
+			Q.push(x->rc);
+		}
+		else if (y)
+		{
+			if (HasChild(*y))
+				Q.push(x->insertAsRC(NULL));
+		}
+	}
+}
